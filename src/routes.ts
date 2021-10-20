dd9293986948ca1ebb936c63c2bcca12eafff568
Natanael Lima <taelima1997@gmail.com>
2021-10-20 11:27:04 -0300

    add route GetLatestMessages

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import { AuthenticateUserController } from './controllers/AuthenticateUserController';
 import { CreateMessageController } from './controllers/CreateMessageController';
+import { GetLatestMessagesController } from './controllers/GetLatestMessagesController';
 import { ProfileController } from './controllers/ProfileController';
 
 import { ensureAuthenticated } from './middlewares/ensureAuthenticted';
@@ -11,11 +12,13 @@ const router = Router();
 const authenticateUserController = new AuthenticateUserController();
 const profileController = new ProfileController();
 const createMessageController = new CreateMessageController();
+const getLatestMessagesController = new GetLatestMessagesController();
 
 router.get('/profile', ensureAuthenticated, profileController.handle);
 
 router.post('/authenticate', authenticateUserController.handle);
 
 router.post('/messages', ensureAuthenticated, createMessageController.handle);
+router.get('/messages', getLatestMessagesController.handle);
 
 export { router }
