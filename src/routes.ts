e0bac0ba795bca570241ceda056f173e9c79e70d
Natanael Lima <taelima1997@gmail.com>
2021-10-20 11:24:11 -0300

    add routes messages post

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { AuthenticateUserController } from './controllers/AuthenticateUserController';
+import { CreateMessageController } from './controllers/CreateMessageController';
 import { ProfileController } from './controllers/ProfileController';
 
 import { ensureAuthenticated } from './middlewares/ensureAuthenticted';
@@ -9,10 +10,12 @@ const router = Router();
  
 const authenticateUserController = new AuthenticateUserController();
 const profileController = new ProfileController();
+const createMessageController = new CreateMessageController();
 
 router.get('/profile', ensureAuthenticated, profileController.handle);
 
 router.post('/authenticate', authenticateUserController.handle);
 
+router.post('/messages', ensureAuthenticated, createMessageController.handle);
 
 export { router }
