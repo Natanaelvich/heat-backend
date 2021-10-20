6676d1c4cecf36069540269a5ca0a5dcebbef9bf
Natanael Lima <taelima1997@gmail.com>
2021-10-20 11:18:51 -0300

    add route profile

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express';
 
 import { AuthenticateUserController } from './controllers/AuthenticateUserController';
+import { ProfileController } from './controllers/ProfileController';
 
 import { ensureAuthenticated } from './middlewares/ensureAuthenticted';
 
 const router = Router();
  
 const authenticateUserController = new AuthenticateUserController();
+const profileController = new ProfileController();
+
+router.get('/profile', ensureAuthenticated, profileController.handle);
 
 router.post('/authenticate', authenticateUserController.handle);
 
